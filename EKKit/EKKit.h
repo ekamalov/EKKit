be807ceb8df6214eaf80fd7f29c1d08f7218d479
Erik Kamalov <ekamalov967@gmail.com>
2019-04-09 14:28:09 +0600

    Initial Commit

---
+++ EKKit/EKKit.h
@@ -0,0 +1,19 @@
+//
+//  EKKit.h
+//  EKKit
+//
+//  Created by Erik Kamalov on 4/9/19.
+//  Copyright © 2019 Neuron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EKKit.
+FOUNDATION_EXPORT double EKKitVersionNumber;
+
+//! Project version string for EKKit.
+FOUNDATION_EXPORT const unsigned char EKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKKit/PublicHeader.h>
+
+
