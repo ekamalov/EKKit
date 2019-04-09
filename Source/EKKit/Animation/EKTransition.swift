ea8032f4ae964118ae35a34df58b63b3ef080215
Erik Kamalov <ekamalov967@gmail.com>
2019-04-09 14:46:36 +0600

    added source

---
+++ Source/EKKit/Animation/EKTransition.swift
@@ -0,0 +1,44 @@
+// The MIT License (MIT)
+// Copyright © 2019 Erik Kamalov (ekamalov967@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import UIKit
+
+public enum EKTransition {
+    
+    public static func transit(_with view:UIView, _ duration: TimeInterval,
+                               animations: (() -> Void)!,delay: TimeInterval = 0,
+                               options:  UIView.AnimationOptions = [],
+                               withComplection completion: (() -> Void)! = {}) {
+        
+        UIView.transition(with: view, duration: duration, options: options, animations: {
+            animations()
+        }, completion: { finished in
+            completion()
+        })
+    }
+}
+
+
+
+
+
+
+
