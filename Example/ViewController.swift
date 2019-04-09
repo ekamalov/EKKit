eb19ecea21d2c15d0648284477656d45afa1557b
Erik Kamalov <ekamalov967@gmail.com>
2019-04-09 15:45:46 +0600

    added example project

---
+++ Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Erik Kamalov on 4/9/19.
+//  Copyright © 2019 Neuron. All rights reserved.
+//
+
+import UIKit
+import EKKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
